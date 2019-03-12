0b2a69655d76d572d32902cea89930517ceb6bfc
Shubham Mittal <mittalsh@amazon.com>
2019-03-11 17:36:04 -0700

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/microsoft/azure/iot/sdk/samples/androidsample/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.microsoft.azure.iot.sdk.samples.androidsample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.iothub.azure.microsoft.com.androidsample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String DeviceConnectionString = "HostName=neha-demo-IoTHub.azure-devices.net;DeviceId=MyAndroidDevice;SharedAccessKey=wi91YW52pqBbAaUJAaFo0OMJ6Ao9xFoDOQjVp7rqMCc=";
+}
